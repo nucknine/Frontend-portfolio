19b36ced5b805ca0456ba5d2a8c33cb2082de54c
nucknine <nucknine@gmail.com>
2017-04-10 18:00:31 +0300

    geolocation

--- Demo_1/app/js/myLoc.js
+++ Demo_1/app/js/myLoc.js
@@ -1,14 +1,57 @@
-var ourCoords = {
-    latitude: 61.778344,
-    longitude: 34.407477
+//функция округления числа до нужного количества знаков
+function roundPlus(x, n) { //x - число, n - количество знаков
+  if(isNaN(x) || isNaN(n)) return false;
+  var m = Math.pow(10,n);
+  return Math.round(x*m)/m;
+}
+
+//опции отслеживания проверка положения каждые 100мс без кэширования и высокая точность
+var options = {
+    enableHightAccuracy: true,
+    timeout: 100,
+    maximumAge: 0
 };
+
+var watchId = null; //переменная для сброса отслеживания
+var map;
+var prevCoords; //координаты для вычисления предыдущей части пути
+
+//координаты компании
+var ourCoords = {
+    lat: 61.778344,
+    lng: 34.407477
+};
+
 window.onload = getMyLocation;
 
 function getMyLocation() {
     if(navigator.geolocation) {
-        navigator.geolocation.getCurrentPosition(displayLocation, displayError);
+        navigator.geolocation.getCurrentPosition(displayLocation, displayError, options);
+
+        var watchButton = document.getElementById("watch");
+        watchButton.onclick = watchLocation;
+
+        var clearWatchButton = document.getElementById("clearWatch");
+        clearWatch.onclick = clearWatch;
+
+        var getRoute = document.getElementById("getRoute");
+        getRoute.onclick = route;
+        // var getRoute = document.getElementById("getRoute");
+        // getRoute.onclick = navigator.geolocation.getCurrentPosition(route, displayError, options);
+
     } else {
         alert("no support geolocation");
+    }
+}
+
+function watchLocation () {
+    watchId = navigator.geolocation.watchPosition(displayLocation, displayError, options);
+}
+
+function clearWatch() {
+    if(watchId) {
+        navigator.geolocation.clearWatch(watchId);
+        watchId = null;
     }
 }
 
@@ -17,13 +60,23 @@ function displayLocation(position) {
     var longitude = position.coords.longitude;
 
     var location = document.getElementById("location");
-    location.innerHTML = "You are at Latitude: " + latitude + ", Longitude: " + longitude;
+    location.innerHTML = "Ваша широта: " + latitude + ", долгота: " + longitude;
+    location.innerHTML += " (определено за " + options.timeout + " миллисекунд)";
 
     var km = computeDistance(position.coords, ourCoords);
     var distance = document.getElementById("distance");
-    distance.innerHTML = "You are " + km + " km from Home";
-
-    showMap(position.coords);
+    distance.innerHTML = "Вы в " + km + " км от нашей компании";
+    distance.innerHTML += " (c " + position.coords.accuracy + " точностью метров)"
+    if (map == null) {
+        showMap(position.coords);
+        prevCoords = position.coords;
+    } else {
+        var meters = computeDistance(position.coords, prevCoords) * 1000;
+        if (meters > 0) {
+            scrollMapToPosition(position.coords);
+            position.coords = null;
+        }
+    }
 }
 
 function displayError(error) {
@@ -39,18 +92,24 @@ function displayError(error) {
     }
     var div = document.getElementById("location");
     div.innerHTML = errorMessage;
+    options.timeout +=100;
+    navigator.geolocation.getCurrentPosition(
+        displayLocation,
+        displayError,
+        options);
+    div.innerHTML += "... попытка с timeout = " + options.timeout + " мс";
 }
 
 //computeDistance////////////////////////////////////////
 function computeDistance (startCoords, destCoords) {
     var startLatRads = degreesToRadians(startCoords.latitude);
     var startLongRads = degreesToRadians(startCoords.longitude);
-    var destLatRads = degreesToRadians(destCoords.latitude);
-    var destLongRads = degreesToRadians(destCoords.longitude);
+    var destLatRads = degreesToRadians(destCoords.lat);
+    var destLongRads = degreesToRadians(destCoords.lng);
 
     var Radius = 6371;
     var distance = Math.acos(Math.sin(startLatRads)*Math.sin(destLatRads)+Math.cos(startLatRads)*Math.cos(destLatRads)*Math.cos(startLongRads - destLongRads))*Radius;
-    return distance;
+    return roundPlus(distance, 2);
 }
 
 function degreesToRadians(degrees) {
@@ -59,13 +118,12 @@ function degreesToRadians(degrees) {
 }
 //////////////////////////////////////////////////////////
 
-var map;
-
 function showMap(coords) {
     var googleLatAndLong = new google.maps.LatLng(coords.latitude, coords.longitude);
 
     var mapOptions = {
-        zoom: 15,
+        scrollwheel: false,
+        zoom: 13,
         center: googleLatAndLong,
         mapTypeId: google.maps.MapTypeId.ROADMAP
     };
@@ -81,8 +139,33 @@ function showMap(coords) {
 //////////////////////////////////////////////////////////
 
 function addMarker(map, latlong, title, content) {
+    //маркер для компании
+    var companyImage = new google.maps.MarkerImage(
+        '../img/map-marker/marker-images/image.png',
+        new google.maps.Size(90,29),
+        new google.maps.Point(0,0),
+        new google.maps.Point(45,29)
+    );
+
+    var companyShape = {
+        coord: [89,0,89,1,89,2,89,3,89,4,89,5,89,6,89,7,89,8,89,9,89,10,89,11,89,12,89,13,89,14,89,15,89,16,89,17,89,18,89,19,89,20,89,21,89,22,89,23,89,24,89,25,89,26,89,27,89,28,0,28,0,27,0,26,0,25,0,24,0,23,0,22,0,21,0,20,0,19,0,18,0,17,0,16,0,15,0,14,0,13,0,12,0,11,0,10,0,9,0,8,0,7,0,6,0,5,0,4,0,3,0,2,0,1,0,0,89,0],
+        type: 'poly'
+    };
+
+    var markerCompanyOptions = {
+        icon: companyImage,
+        shape: companyShape,
+        position: ourCoords,
+        map: map,
+        title: "Наша компания",
+        clickable: true
+    };
+
+    var markerCompany = new google.maps.Marker(markerCompanyOptions);
+
+    //маркер вашего местоположения
     var markerOptions = {
-        postition: latlong,
+        position: latlong,
         map: map,
         title: title,
         clickable: true
@@ -90,14 +173,48 @@ function addMarker(map, latlong, title, content) {
 
     var marker = new google.maps.Marker(markerOptions);
 
-    var infoWindowOptions = {
-        content: content,
-        position: latlong
+  var infoWindowOptions = {
+    content: content,
+    position: latlong
+  };
+
+  var infoWindow = new google.maps.InfoWindow(infoWindowOptions);
+
+  google.maps.event.addListener(marker, "click", function(){
+    infoWindow.open(map);
+        });
+}
+
+function scrollMapToPosition(coords) {
+    var latitude = coords.latitude;
+    var longitude = coords.longitude;
+    var latlong = new google.maps.LatLng(latitude, longitude);
+
+    map.panTo(latlong);
+
+    addMarker(map, latlong, "Ваше новое положение ", "Вы переместились: " + latitude + ", " + longitude);
+}
+
+function route() {
+    // var latitude = position.coords.latitude;
+    // var longitude = position.coords.longitude;
+
+    // console.log(latitude, longitude);
+
+    var directionsService = new google.maps.DirectionsService;
+    var directionsDisplay = new google.maps.DirectionsRenderer;
+
+    var request = {
+        origin: new google.maps.LatLng(33, 44), //точка старта
+        destination: new google.maps.LatLng(44,61), //точка финиша
+        travelMode: google.maps.DirectionsTravelMode.DRIVING //режим прокладки маршрута
     };
 
-    var infoWindow = new google.maps.InfoWindow(infoWindowOptions);
-
-    google.maps.event.addListener(marker, "click", function(){
-        infoWindow.open(map);
+    directionsService.route(request, function(response, status) {
+        if (status == google.maps.DirectionsStatus.OK) {
+            directionsDisplay.setDirections(response);
+        }
     });
+
+    directionsDisplay.setMap(map);
 }
