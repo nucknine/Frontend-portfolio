50bf3ddd8bc1d2a82e52105c6b72bc7749aecb0a
nucknine <nucknine@gmail.com>
2018-06-28 14:59:54 +0300

    hero section done about page in progress

--- portfolio-page/source/js/app.js
+++ portfolio-page/source/js/app.js
@@ -1,4 +1,3 @@
-(function() {
 'use strict';
 
 svg4everybody();
@@ -7,23 +6,20 @@ var first = 'Oleg'
 var name = `Your name is ${first}.`
 
 class baseModel {
-constructor(options = {}, data = []) { // class constructor
-this.name = 'Base'
-this.url = 'http://azat.co/api'
-this.data = data
-this.options = options
-}
+    constructor(options = {}, data = []) { // class constructor
+        this.name = 'Base'
+        this.url = 'http://azat.co/api'
+        this.data = data
+        this.options = options
+    }
 
-getName() { // class method
-console.log(`Class name: ${this.name}`)
-}
+    getName() { // class method
+        console.log(`Class name: ${this.name}`)
+    }
 }
 
 var obj = new baseModel();
 
-console.log('es6' + obj.getName() + ' ' + name);
+console.log(name);
+obj.getName();
 
-setTimeout(function() {
-document.querySelector('.greating_picture').classList.add('m--show');
-}, 1000);
-})();
