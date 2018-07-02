d2cdd696c0e0b2bd886f22b35b0dad08eef459c9
nucknine <nucknine@gmail.com>
2018-07-02 11:42:07 +0300

    map

--- portfolio-page/source/js/app.js
+++ portfolio-page/source/js/app.js
@@ -2,24 +2,120 @@
 
 svg4everybody();
 
-var first = 'Oleg'
-var name = `Your name is ${first}.`
+function initMap() {
+    var myLatlng = new google.maps.LatLng(40.674, -73.945);
 
-class baseModel {
-    constructor(options = {}, data = []) { // class constructor
-        this.name = 'Base'
-        this.url = 'http://azat.co/api'
-        this.data = data
-        this.options = options
-    }
-
-    getName() { // class method
-        console.log(`Class name: ${this.name}`)
-    }
+    var map = new google.maps.Map(
+        document.getElementById('map'), {
+            zoom: 12,
+            center: myLatlng,
+            styles: [
+                {
+                    'featureType': 'landscape',
+                    'stylers': [
+                        {
+                            'hue': '#FFBB00'
+                        },
+                        {
+                            'saturation': 43.400000000000006
+                        },
+                        {
+                            'lightness': 37.599999999999994
+                        },
+                        {
+                            'gamma': 1
+                        }
+                    ]
+                },
+                {
+                    'featureType': 'road.highway',
+                    'stylers': [
+                        {
+                            'hue': '#FFC200'
+                        },
+                        {
+                            'saturation': -61.8
+                        },
+                        {
+                            'lightness': 45.599999999999994
+                        },
+                        {
+                            'gamma': 1
+                        }
+                    ]
+                },
+                {
+                    'featureType': 'road.arterial',
+                    'stylers': [
+                        {
+                            'hue': '#FF0300'
+                        },
+                        {
+                            'saturation': -100
+                        },
+                        {
+                            'lightness': 51.19999999999999
+                        },
+                        {
+                            'gamma': 1
+                        }
+                    ]
+                },
+                {
+                    'featureType': 'road.local',
+                    'stylers': [
+                        {
+                            'hue': '#FF0300'
+                        },
+                        {
+                            'saturation': -100
+                        },
+                        {
+                            'lightness': 52
+                        },
+                        {
+                            'gamma': 1
+                        }
+                    ]
+                },
+                {
+                    'featureType': 'water',
+                    'stylers': [
+                        {
+                            'hue': '#0078FF'
+                        },
+                        {
+                            'saturation': -13.200000000000003
+                        },
+                        {
+                            'lightness': 2.4000000000000057
+                        },
+                        {
+                            'gamma': 1
+                        }
+                    ]
+                },
+                {
+                    'featureType': 'poi',
+                    'stylers': [
+                        {
+                            'hue': '#00FF6A'
+                        },
+                        {
+                            'saturation': -1.0989010989011234
+                        },
+                        {
+                            'lightness': 11.200000000000017
+                        },
+                        {
+                            'gamma': 1
+                        }
+                    ]
+                }
+            ]
+        });
+        // The marker, positioned at Uluru
+    var marker = new google.maps.Marker({ position: myLatlng, map: map });
 }
 
-var obj = new baseModel();
-
-console.log(name);
-obj.getName();
-
+initMap();
